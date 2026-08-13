bed3de11eae21522cf31486c22c7d7b1c5ad89ef
jollyroger182 <hello@jollyy.dev>
2026-08-13 13:39:47 -0400

    add node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,21 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
+  site: import.meta.env.EXTERNAL_URL || "https://haven.hackclub.com",
+
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+
+  output: "server",
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
